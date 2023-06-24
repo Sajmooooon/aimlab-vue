af3b31370c3642667eb3cf6359f9545ba5f56d7f
simon <sajmoooonsk@gmail.com>
2023-06-24 14:43:45 +0200

    feat: generate balloons

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
+  <Balloon v-for="balloon in balloons" :key="balloon.id"
+           :x="balloon.x" :y="balloon.y"
+  ></Balloon>
 </template>
 
 <script>
-
+import Balloon from "@/components/Balloon";
 export default {
   name: 'App',
   components: {
+    Balloon
+  },
+  data(){
+    return{
+      maxWidth: 0,
+      maxHeight: 0,
+      balloons: [
+        {id: 0, x: 0, y: 0},
+        {id: 1, x: 50, y: 150},
+        {id: 2, x: 150, y: 50},
+        {id: 3, x: 300, y: 400},
+      ],
+    }
+  },
+  beforeMount() {
+    const body = document.querySelector('body');
 
+    const bodyHeight = body.offsetHeight;
+    const bodyWidth = body.offsetWidth;
+
+    this.maxHeight = bodyHeight
+    this.maxWidth = bodyWidth
   }
 }
 </script>
@@ -18,5 +42,8 @@ body{
   background-position: center center;
   background-size: cover;
   background-attachment: fixed;
+  height: 100vh;
+  cursor: crosshair;
+
 }
 </style>
