80243a8c5d430d30587ac6c0d04e1c5545ec0462
simon <sajmoooonsk@gmail.com>
2023-06-24 14:25:12 +0200

    feat: create balloon component

---
+++ src/components/Balloon.vue
@@ -0,0 +1,23 @@
+<template>
+  <img src="../assets/balloon.png" alt="balloon" class="balloon" ref="balloon">
+</template>
+
+<script>
+export default {
+  name: "Balloon",
+  props: ['x','y'],
+  mounted() {
+    const img = this.$refs.balloon
+    img.style.left = this.x + 'px';
+    img.style.top = this.y + 'px'
+  }
+}
+</script>
+
+<style scoped>
+.balloon{
+  height: 104px;
+  width: 106px;
+  position: absolute;
+  filter: drop-shadow(0 0 0.75rem gray);}
+</style>
